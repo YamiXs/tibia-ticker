3d6352fb6aae1b0aaa6f3f0a1ee1805b602dadd6
YamiXs <pontus.g.lindblad@gmail.com>
2025-06-26 22:26:42 +0200

    Update boosted-proxy.js

--- boosted-proxy.js
+++ boosted-proxy.js
@@ -5,13 +5,13 @@
     const res1 = await fetch(proxy + encodeURIComponent('https://www.tibia.com/library/?subtopic=creatures'));
     const html1 = (await res1.json()).contents;
     const m1 = html1.match(/<td.*?>Today's boosted creature:<\/.*?><td.*?><.*?>([^<]+)<\/.*?>/i);
-if (m1) result.creature = m1[1];
+    if (m1) result.creature = m1[1];
     if (m1) result.creature = m1[1];
 
     const res2 = await fetch(proxy + encodeURIComponent('https://www.tibia.com/library/?subtopic=boostablebosses'));
     const html2 = (await res2.json()).contents;
     const m2 = html2.match(/<td.*?>Today's boosted boss:<\/.*?><td.*?><.*?>([^<]+)<\/.*?>/i);
-if (m2) result.boss = m2[1];
+    if (m2) result.boss = m2[1];
     if (m2) result.boss = m2[1];
 
   } catch(e) {
