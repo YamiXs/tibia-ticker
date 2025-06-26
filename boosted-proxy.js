6aaf8ee2ae40799e42f00c1e8015b2b05b6dc4af
YamiXs <pontus.g.lindblad@gmail.com>
2025-06-26 22:26:19 +0200

    Update boosted-proxy.js

--- boosted-proxy.js
+++ boosted-proxy.js
@@ -4,12 +4,14 @@
     const proxy = 'https://api.allorigins.win/get?url=';
     const res1 = await fetch(proxy + encodeURIComponent('https://www.tibia.com/library/?subtopic=creatures'));
     const html1 = (await res1.json()).contents;
-    const m1 = html1.match(/Today's boosted creature:\s*<\/b>.*?<nobr>([^<]+)<\/nobr>/i);
+    const m1 = html1.match(/<td.*?>Today's boosted creature:<\/.*?><td.*?><.*?>([^<]+)<\/.*?>/i);
+if (m1) result.creature = m1[1];
     if (m1) result.creature = m1[1];
 
     const res2 = await fetch(proxy + encodeURIComponent('https://www.tibia.com/library/?subtopic=boostablebosses'));
     const html2 = (await res2.json()).contents;
-    const m2 = html2.match(/Today's boosted boss:\s*<\/b>.*?<nobr>([^<]+)<\/nobr>/i);
+    const m2 = html2.match(/<td.*?>Today's boosted boss:<\/.*?><td.*?><.*?>([^<]+)<\/.*?>/i);
+if (m2) result.boss = m2[1];
     if (m2) result.boss = m2[1];
 
   } catch(e) {
